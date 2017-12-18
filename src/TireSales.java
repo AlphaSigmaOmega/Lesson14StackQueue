14a8ff1920e26a492cc334e03c3f0d9770a5dba8
john7770 <john7770@ISS2V2PKH2.scdsb.pri>
2017-12-18 10:28:55 -0500

    Tire sales boi

--- src/TireSales.java
+++ src/TireSales.java
@@ -24,7 +24,6 @@ public class TireSales {
         }
         System.out.println("Next tire to be sold is " + pole.peekTop());
         System.out.println("Number of tires left on pole is " + pole.size());
-        
     }
 
 }
